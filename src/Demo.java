da3897ed6b9c27e0608ae41c422303ba9d927f50
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-09-20 20:33:26 +0200

    Add ArrayList of connected peers in Dashboard

--- src/Demo.java
+++ src/Demo.java
@@ -4,20 +4,42 @@ import peer.BasePeer;
 import peer.Producer;
 import peer.exception.RunnableException;
 
+import java.util.Random;
+
 public class Demo {
     public static void main(String[] args){
         IDashboard dashboard = new Dashboard();
-        BasePeer c1 = new Producer(dashboard,
-                "@DavideColombo");
+        BasePeer p1 = new Producer(dashboard,
+                "@VermicelloPazzerello");
+        BasePeer p2 = new Producer(dashboard,
+                "@Lumachina29");
+        BasePeer p3 = new Producer(dashboard,
+                "@LaMuccaMuu");
+
+        Random rand = new Random();
 
         try {
-            c1.runInstance();
+            p1.runInstance();
+            p2.runInstance();
+            p3.runInstance();
             Thread.sleep(10000);
-            c1.pause();
+            p1.pause();
+            Thread.sleep((long) (1000 * rand.nextDouble()));
+            p2.pause();
+            Thread.sleep((long) (1000 * rand.nextDouble()));
+            p3.pause();
             Thread.sleep(5000);
-            c1.resume();
+            p1.resume();
+            Thread.sleep((long) (1000 * rand.nextDouble()));
+            p2.resume();
+            Thread.sleep((long) (1000 * rand.nextDouble()));
+            p3.resume();
             Thread.sleep(10000);
-            c1.stop();
+            p1.stop();
+            Thread.sleep((long) (1000 * rand.nextDouble()));
+            p2.stop();
+            Thread.sleep((long) (1000 * rand.nextDouble()));
+            p3.stop();
         } catch(InterruptedException | RunnableException e) {
             e.printStackTrace();
         }
